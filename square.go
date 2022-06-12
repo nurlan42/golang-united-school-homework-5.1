d9e771d386c80d17a1a0f34f8e3a8ec5d152c0cc
Nurlan <ikhsan.nurlan@gmail.com>
2022-06-12 12:18:04 +0600

    merge: master

--- square.go
+++ square.go
@@ -1,35 +1,16 @@
 package square
 
-<<<<<<< HEAD
-=======
 type Square struct {
 	start Point
 	a     uint
 	end   Point
 }
 
->>>>>>> master
+
 type Point struct {
 	x, y int
 }
 
-<<<<<<< HEAD
-type Square struct {
-	start Point
-	a     uint
-}
-
-func (receiver) End() Point {
-	// implement me
-}
-
-func (receiver) Area() uint {
-	// implement me
-}
-
-func (receiver) Perimeter() uint {
-	// implement me
-=======
 func (s *Square) End() Point {
 	s.end = Point{x: s.start.x + int(s.a), y: s.start.y + int(s.a)}
 	return s.end
@@ -40,5 +21,4 @@ func (s *Square) Perimeter() uint {
 
 func (s *Square) Area() uint {
 	return s.a * s.a
->>>>>>> master
 }
